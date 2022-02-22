513854739424e3376826d11e7c3313d98ac51c71
Andrea <Andrea@Pasqualino>
2022-02-22 15:25:36 +0100

    es1, es2 array

--- Esercitazioni/src/Esercitazione1/Esercitazione1.java
+++ Esercitazioni/src/Esercitazione1/Esercitazione1.java
@@ -4,16 +4,23 @@ public class Esercitazione1 {
 	
 
 	public static void main(String[] args) {
-		
+		//test es1 array
 		int [] ai1 = {1,2,3,4,5,6,7,8,9,10};
 		stampaPrimoUltimo(ai1);
 		System.out.println();
 		
 		int [] ai2 = {1,2,3,4,5,6,7,8,9};
 		stampaPrimoUltimo(ai2);
+		System.out.println();
+		
+		//test es2 array
+		int [] ai3 = {1,2,3,3,1};		
+		pariEDispari(ai3);		
+		System.out.println();
+		
 
 	}
-	
+	//es1 array
 	public static void stampaPrimoUltimo(int[] ai) {
 		if (ai.length%2==0) //se la lunghezza è pari
 			for (int i=0; i<ai.length/2; ++i) {
@@ -29,5 +36,25 @@ public class Esercitazione1 {
 		}
 			
 	}
-
+	
+	//es2 array
+	public static void pariEDispari (int[] ai) {
+		int sommaPari=0;
+		int sommaDispari=0;
+		
+			for (int i=0; i<ai.length; ++i) {
+				if(i%2==0)
+					sommaPari+=ai[i];
+				else
+					sommaDispari+=ai[i];
+			}
+		
+		System.out.println(sommaPari);
+		System.out.println(sommaDispari);
+		
+		if (sommaPari==sommaDispari)
+			System.out.println("Pari e dispari uguali");
+		else
+			System.out.println("Pari e dispari diversi");
+	}				
 }
