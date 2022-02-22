b09048990e49fc924a66280c372086eb2ab83d59
Andrea <Andrea@Pasqualino>
2022-02-22 14:57:56 +0100

    Inizio primo es

--- Esercitazioni/src/Esercitazioni1/Esercitazione1.java
+++ Esercitazioni/src/Esercitazioni1/Esercitazione1.java
@@ -1,10 +1,33 @@
 package Esercitazioni1;
 
 public class Esercitazione1 {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int [] ai1 = {1,2,3,4,5,6};
+		stampaPrimoUltimo(ai1);
+		System.out.println();
+		
+		int [] ai2 = {1,2,3,4,5,6,7};
+		stampaPrimoUltimo(ai2);
 
+	}
+	
+	public static void stampaPrimoUltimo(int[] ai) {
+		if (ai.length%2==0) //se la lunghezza è pari
+			for (int i=0; i<ai.length/2; ++i) {
+				System.out.println(ai[i]);
+				System.out.println(ai[ai.length-1-i]);
+			}
+		else {              //se la lunghezza è dispari
+			for (int i=0; i<ai.length/2; ++i) {
+				System.out.println(ai[i]);
+				System.out.println(ai[ai.length-1-i]);
+			}
+			System.out.println(ai[ai.length/2]);
+		}
+			
 	}
 
 }
