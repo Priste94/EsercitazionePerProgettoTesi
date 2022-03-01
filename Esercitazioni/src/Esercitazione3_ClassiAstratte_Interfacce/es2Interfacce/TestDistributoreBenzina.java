e8bae5d3e7e1992c07da328a060849894d5b130c
Andrea <Andrea@Pasqualino>
2022-03-01 16:03:25 +0100

    Corretti problemi input utente es classi astratte

--- Esercitazioni/src/Esercitazione3_ClassiAstratte_Interfacce/es2Interfacce/TestDistributoreBenzina.java
+++ Esercitazioni/src/Esercitazione3_ClassiAstratte_Interfacce/es2Interfacce/TestDistributoreBenzina.java
@@ -14,14 +14,18 @@ public class TestDistributoreBenzina {
 		
 		DistributoreDiBenzina[] T = {db1, db2, db3};
 		
-		int i,ch=0,scelta,var,scelta2=0;
-		
+		int i,ch=0,var,scelta2=0, scelta3=0;
+		String scelta="";
 		System.out.println("Seleziona il distributore");
 		for(i=0;i < T.length;i++)
 		   System.out.println(i+"]"+T[i].getProprietario());
-		ch = scanner.nextInt();
-		if(ch < 0 || ch >= T.length)
-			ch = 0;
+		
+		while(!scanner.hasNextInt() || scanner == null) {
+			System.out.println("Rispondi con un valore intero!");
+			scelta = scanner.next();		 
+		}
+		scelta = scanner.next();
+		ch = Integer.parseInt(scelta);
 		//Giuseppe è distributore di default
 		
 		System.out.println();
@@ -32,27 +36,27 @@ public class TestDistributoreBenzina {
 				+ "4)Comparazione");
 		
 			System.out.println("Inserisci la tua scelta: ");
-			scelta=scanner.nextInt();
-			switch(scelta){
+			scelta2=scanner.nextInt();
+			switch(scelta2){
 				case 1:
 					System.out.printf("Ins.num.litri da prelevare ,contenuta: "+
-					T[scelta].getContenuta()+" litri: ");
-					T[scelta].erogazioneCarburante(scanner.nextInt());
+					T[scelta2].getContenuta()+" litri: ");
+					T[scelta2].erogazioneCarburante(scanner.nextInt());
 			
 					System.out.printf("Vuoi continuare?\nDigita 0 per confermare oppure 1 per negare: ");
-					scelta2=scanner.nextInt();
+					scelta3=scanner.nextInt();
 					break;
 				case 2:
-					System.out.printf("il distrubutore contiene "+T[scelta].getContenuta()+" su "+T[ch].getCapacita());
+					System.out.printf("il distrubutore contiene "+T[scelta2].getContenuta()+" su "+T[ch].getCapacita());
 					T[ch].rifornisciDistributore();
 					System.out.printf(" Vuoi continuare?\nDigita 0 per confermare oppure 1 per negare: ");
 					
-					scelta2=scanner.nextInt();
+					scelta3=scanner.nextInt();
 					break;
 				case 3: 
 					System.out.printf("L'incasso totale di oggi:"+T[ch].getBilancio());
 					System.out.printf("Vuoi continuare?\nDigita 0 per confermare oppure 1 per negare: ");
-					scelta2=scanner.nextInt();
+					scelta3=scanner.nextInt();
 					break;
 				case 4:
 					DistributoreDiBenzina Dch = T[ch];
